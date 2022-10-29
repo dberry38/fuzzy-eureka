f1da5e9a999f2e81eedca0e04fd780329e849450
dberry38 <108836058+dberry38@users.noreply.github.com>
2022-10-28 18:19:34 -0700

    Initial Commit

---
+++ Develop/express.js
@@ -0,0 +1,22 @@
+
+const express = require('express');
+const app = express();
+
+
+
+// we need HTML routes for get notes.
+// GET * should return the index.html file
+
+
+// need API routes 
+// GET /api/notes should read the db.json file and return all saved notes as JSON
+// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. 
+
+// You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+
+
+
+
+
+// bonus shmonus
+// add that DELETE my dude
