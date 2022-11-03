59278eef4b5d74ac9b2a49449512b0be0f903bac
dberry38 <108836058+dberry38@users.noreply.github.com>
2022-11-02 18:23:57 -0700

    Adding server.js functionality

--- Develop/server.js
+++ Develop/server.js
@@ -1,13 +1,20 @@
 
 const express = require('express');
+const fs = require('fs');
 const notesData = require('./db/db.json');
-// const notesPage = require('./public/notes.html')
+const { clog } = require('./middleware/clog');
+const api = require('./routes/index.js');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(clog);
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+app.use(express.static('public'));
 
 
 // we need HTML routes for get notes.
@@ -25,11 +32,17 @@ res.sendFile(path.join(__dirname, '/public/index.html'))
 // GET /api/notes should read the db.json file and return all saved notes as JSON
 app.get('/api/notes', (req, res) => res.json(notesData));
 
-// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. 
-app.post('/api/notes', (req, res) => );
+// todo POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. 
 
-// You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+// app.post('/api/notes', (req, res) => );
 
+// todo You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
 
 
 
